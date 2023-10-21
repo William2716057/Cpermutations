75321948d75d5679725bc7fd1c823c064fb59037
William Spriggs <Willspri@gmail.com>
2023-10-21 19:46:04 +1300

    updated

--- Cpermutations/Cpermutations.cpp
+++ Cpermutations/Cpermutations.cpp
@@ -8,11 +8,31 @@
 //    std::cout << "Hello World!\n";
 //}
 
+//swap values of two variables using temp variable 
 void swap(char *x, char* y) {
 	char temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
+void perms(char* lst, int start, int end) {
+	if (start == end) {
+		printf("%s\n", lst);
+	}
+	else {
+		for (int i = start; i <= end; i++) {
+			swap(&lst[start], &lst[i]);
+			perms(lst, start + 1, end);
+			swap(&lst[start], &lst[i]);
+		}
+	}
+}
 
+int main() {
+	char lst[100]; //array for storing input
+	printf("enter numbers: ");
+	scanf("%s", lst);
+	perms(lst, 0, n - 1);
+	return 0;
+}
 
