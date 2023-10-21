eca5594ac2d7e9512188474fa94823a031d77d94
William Spriggs <Willspri@gmail.com>
2023-10-21 19:36:24 +1300

    Add project files.

---
+++ Cpermutations/Cpermutations.cpp
@@ -0,0 +1,18 @@
+// Cpermutations.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
+
+void swap(char *x, char* y) {
+	char temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+
+
